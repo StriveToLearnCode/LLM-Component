f81b28cccbdf55f375aa1871c44c3de61395076a
StriveToLearnCode <xinyahuang09@qq.com>
2024-11-30 15:19:00 +0800

    完善内联对话框1

---
+++ src/components/InlineDialog.vue
@@ -0,0 +1,178 @@
+<template>
+  <div class="inline-dialog" v-if="isShow">
+    <div class="top">
+      <input type="text" placeholder="What are you searching for?" />
+      <div class="Esc" @click="close">
+        <span>Esc</span>
+      </div>
+    </div>
+    <div class="middle" @click="enterChat">
+      <div class="ask">
+        <i class="iconfont">&#xe7ca;</i>
+        <span>Ask</span>
+      </div>
+      <div>
+        <span>Experimental</span>
+      </div>
+    </div>
+    <div class="bottom">
+      <div class="title">Suggestions</div>
+      <div class="list">
+        <div class="item">
+          <i class="iconfont">&#xe663;</i>
+          <span>Get Started with Claude</span>
+        </div>
+        <div class="item">
+          <i class="iconfont">&#xe663;</i>
+          <span>Get Started with Claude</span>
+        </div>
+        <div class="item">
+          <i class="iconfont">&#xe663;</i>
+          <span>Get Started with Claude</span>
+        </div>
+        <div class="item">
+          <i class="iconfont">&#xe663;</i>
+          <span>Get Started with Claude</span>
+        </div>
+        <div class="item">
+          <i class="iconfont">&#xe663;</i>
+          <span>Get Started with Claude</span>
+        </div>
+        <div class="item">
+          <i class="iconfont">&#xe663;</i>
+          <span>Get Started with Claude</span>
+        </div>
+        <div class="item">
+          <i class="iconfont">&#xe663;</i>
+          <span>Get Started with Claude</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <AskChat v-else></AskChat>
+</template>
+<script setup>
+import { ref } from "vue";
+import AskChat from "./arkChat.vue";
+const emit = defineEmits(["close"]);
+const isShow = ref(true);
+const close = () => {
+  emit("close", false);
+};
+const enterChat = () => {
+  isShow.value = false;
+};
+</script>
+<style scoped lang="scss">
+.inline-dialog {
+  border: 1px solid #2a2a2a;
+  border-radius: 10px;
+  width: 550px;
+  height: 400px;
+  background-color: #0a0a0a;
+  font-size: 14px;
+  .top {
+    position: relative;
+    input {
+      width: 100%;
+      height: 50px;
+      border-radius: 10px 10px 0 0;
+      background-color: #0a0a0a;
+      border-color: #2a2a2a;
+      color: #fff;
+      padding: 10px;
+    }
+    input:focus {
+      outline: none;
+    }
+    .Esc {
+      position: absolute;
+      cursor: pointer;
+      right: 10px;
+      top: 10px;
+      padding: 5px 8px;
+      font-size: 14px;
+      border-radius: 10px;
+      border: 1px solid #2a2a2a;
+      span {
+        color: #fff;
+      }
+    }
+  }
+
+  .middle {
+    display: flex;
+    justify-content: space-between;
+    cursor: pointer;
+    margin: 8px;
+    padding: 0 10px;
+    height: 40px;
+    border-color: #2a2a2a;
+    text-align: center;
+    line-height: 40px;
+    .ask {
+      border-color: #2a2a2a;
+      span {
+        color: #fff;
+        margin-left: 8px;
+      }
+      .inconfont {
+        background-color: #0a0a0a;
+        border-color: #2a2a2a;
+        color: #fff;
+      }
+    }
+    .experimental {
+      width: 100px;
+      border-radius: 10px;
+      text-align: center;
+      border-color: #2a2a2a;
+      span {
+        color: #fff;
+      }
+    }
+  }
+  .middle:hover {
+    background-color: #2a2a2a;
+    border-color: #2a2a2a;
+    border-radius: 5px;
+  }
+  .bottom {
+    .title {
+      margin-left: 10px;
+      line-height: 20px;
+      height: 20px;
+      color: #949494;
+    }
+    .item {
+      margin: 8px;
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+      border-radius: 5px;
+      height: 40px;
+      font-size: 14px;
+      line-height: 40px;
+      .iconfont {
+        margin-right: 8px;
+        font-size: 18px;
+      }
+      span {
+        color: #fff;
+      }
+    }
+    .item:hover {
+      background-color: #2a2a2a;
+    }
+    .list {
+      height: 250px;
+      overflow: auto;
+      .item {
+        padding: 10px;
+        border-color: #2a2a2a;
+        color: #fff;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/arkChat.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="ask-container">
+    <div class="top">
+      <div class="headder">
+        <div class="left">
+          <div class="universal">Universal Search</div>
+          <div class="ask">
+            <span>Ask</span>
+            <i class="iconfont">&#xe7ca;</i>
+          </div>
+        </div>
+        <div class="right">
+          <div>Experimental</div>
+        </div>
+      </div>
+      <div class="input">
+        <div>
+          <span>Ark</span>
+          <i class="iconfont">&#xe7ca;</i>
+        </div>
+        <input type="text" />
+      </div>
+    </div>
+    <div class="content">
+      <!-- 用户 -->
+      <div v-for="(item, index) in messageArr" :key="index">
+        <div class="left" v-if="item.role === 'user'">
+          <div class="user">
+            <div class="avatar">
+              <img src="@/assets/user.png" alt="User Avatar" />
+            </div>
+            <div class="text">{{ item.content }}</div>
+          </div>
+        </div>
+        <!-- AI -->
+        <div class="right" v-else>
+          <div class="Ai">
+            <div class="text" v-html="renderMarkdown(item.content)"></div>
+            <div class="avatar">
+              <img src="@/assets/chatgpt.jpg" alt="AI Avatar" />
+            </div>
+          </div>
+          <div class="tools">
+            <div class="copy" @click="copyMessage(item.content)">
+              <i class="iconfont">&#xe706;</i>
+              <span>Copy</span>
+            </div>
+            <div class="retry">
+              <i class="iconfont">&#xe774;</i>
+              <span>Retry</span>
+            </div>
+            <div class="like">
+              <i class="iconfont">&#xec7f;</i>
+            </div>
+            <div class="dislike">
+              <i class="iconfont">&#xe62d;</i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss">
+.ask-container {
+  width: 400px;
+  height: 100%;
+  background-color: #0a0a0a;
+  color: #a6a6a6;
+  border-radius: 20px;
+  border: 1px solid #2a2a2a;
+  padding: 10px;
+  .top {
+    border-bottom: 1px solid #2a2a2a;
+    .headder {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 10px;
+      font-size: 14px;
+      .left {
+        display: flex;
+        gap: 10px;
+      }
+    }
+    .input {
+      position: relative;
+      background-color: #0a0a0a;
+      div {
+        position: absolute;
+        left: 10px;
+        top: 50%;
+        transform: translateY(-50%);
+        span {
+          margin-right: 5px;
+        }
+      }
+      input {
+        background-color: #0a0a0a;
+        padding: 5px 10px 5px 70px;
+        width: 300px;
+        border: none;
+      }
+      input:focus {
+        outline: none;
+      }
+    }
+  }
+  .content {
+    width: 100%;
+    height: 100px;
+    overflow: auto;
+  }
+}
+</style>
